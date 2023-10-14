77936b2f4b5c2e181e298c384633421a6cd91233
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-10-14 21:46:16 +0300

    tests for service

--- internal/service/secret_test.go
+++ internal/service/secret_test.go
@@ -5,12 +5,16 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel"
+
 	"github.com/vodolaz095/purser/internal/repository"
 	"github.com/vodolaz095/purser/internal/repository/memory"
 	"github.com/vodolaz095/purser/internal/repository/mysql"
 	"github.com/vodolaz095/purser/internal/repository/postgresql"
 	"github.com/vodolaz095/purser/internal/repository/redis"
-	"go.opentelemetry.io/otel"
+	"github.com/vodolaz095/purser/model"
+	"github.com/vodolaz095/purser/pkg"
 )
 
 func secretServiceTester(t *testing.T, repo repository.SecretRepo) {
@@ -21,6 +25,12 @@ func secretServiceTester(t *testing.T, repo repository.SecretRepo) {
 		t.Errorf("error initializing repo: %s", err)
 		return
 	}
+	defer func() {
+		err = repo.Close(ctx)
+		if err != nil {
+			t.Errorf("error closing repo: %s", err)
+		}
+	}()
 	ss := SecretService{
 		Tracer: otel.Tracer("unit_test_service"),
 		Repo:   repo,
@@ -30,12 +40,51 @@ func secretServiceTester(t *testing.T, repo repository.SecretRepo) {
 		t.Errorf("error pinging repo: %s", err)
 		return
 	}
-	// TODO - other methods
-
-	err = repo.Close(ctx)
+	secret, err := ss.Create(ctx, "test secret", map[string]string{
+		"a": "b",
+	})
 	if err != nil {
-		t.Errorf("error closing repo: %s", err)
+		t.Errorf("error creating secret: %s", err)
+		return
 	}
+	found, err := ss.FindByID(ctx, secret.ID)
+	if err != nil {
+		t.Errorf("error creating secret: %s", err)
+		return
+	}
+	assert.Equal(t, secret.ID, found.ID, "id differs")
+	assert.Equal(t, secret.Body, found.Body, "body differs")
+	assert.Equal(t, secret.Meta, found.Meta, "meta differs")
+
+	empty, err := ss.FindByID(ctx, pkg.UUID())
+	if err != nil {
+		if err != model.ErrSecretNotFound {
+			t.Errorf("wrong error: %s", err)
+			return
+		}
+	}
+	assert.Equal(t, empty.ID, "", "id differs")
+	assert.Equal(t, empty.Body, "", "body differs")
+	assert.Equal(t, len(empty.Meta), 0, "meta differs")
+
+	err = ss.DeleteByID(ctx, secret.ID)
+	if err != nil {
+		t.Errorf("error deleting: %s", err)
+		return
+	}
+
+	empty, err = ss.FindByID(ctx, secret.ID)
+	if err != nil {
+		if err != model.ErrSecretNotFound {
+			t.Errorf("wrong error: %s", err)
+			return
+		}
+	} else {
+		t.Error("no error thrown for record not found")
+	}
+	assert.Equal(t, empty.ID, "", "id differs")
+	assert.Equal(t, empty.Body, "", "body differs")
+	assert.Equal(t, len(empty.Meta), 0, "meta differs")
 }
 
 func TestSecretServiceMemory(t *testing.T) {
